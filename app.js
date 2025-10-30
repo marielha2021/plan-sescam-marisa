ca13cb9e8fc0421f31c150ee782c0dfb1b1984a7
marielha2021 <marielha2021@gmail.com>
2025-10-30 18:01:45 +0100

    Update error alert message format
    Change alert message format for displaying errors.

--- app.js
+++ app.js
@@ -258,7 +258,6 @@ qs("#start-sim").addEventListener("click", ()=>{
 qs("#view-errors").addEventListener("click", ()=>{
   const errors = (state.tests && state.tests.errors) || [];
   if(!errors.length) alert("No tienes errores registrados aún.");
-  else alert("Errores: 
 " + errors.join("
 "));
 });
